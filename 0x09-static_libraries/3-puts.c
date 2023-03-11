f2376c99b1338d93f49640eee46b91c733fde081
AmedBah <Amedbah2000@gmail.com>
2023-03-10 16:19:05 -0800

    commit

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -5,9 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-
-int main(void)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
+
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+_puts("\"At the end of the day,the best hacker\"\n\t- Kevin Mitnick");
+return (0);
+}
