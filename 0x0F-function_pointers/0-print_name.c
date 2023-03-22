04526208fb5e0f16788ae6ae468905654fa19952
AmedBah <86324218+AmedBah@users.noreply.github.com>
2023-03-22 19:14:56 +0000

    commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- *print_name - prints a name as is
- * @name: name of the person
- *@f:a pointer
- * Return: Nothing.
+ * print_name - This function prints a name using function pointers.
+ * @name: String to be printed out
+ * @f: Function poniter
+ *
+ * Return: Null
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	void (*ptr)(char *);
 
 	if (name == NULL || f == NULL)
 		return;
-	ptr=f;
+
+	ptr = f;
 	ptr(name);
 }
