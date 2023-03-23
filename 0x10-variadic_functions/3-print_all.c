df151b6d454cbbc37809aaa7ed2914218968f816
AmedBah <86324218+AmedBah@users.noreply.github.com>
2023-03-23 23:50:31 +0000

    commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,7 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
 /**
  * print_all - a function that prints anything
  * @format: a list of types of arguments passed to the function
