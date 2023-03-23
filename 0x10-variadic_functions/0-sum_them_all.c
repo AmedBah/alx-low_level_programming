45b9190e1c6d0051bfb0edc3d9f788b73bb57ab4
AmedBah <86324218+AmedBah@users.noreply.github.com>
2023-03-23 23:00:03 +0000

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,18 +11,18 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    int i, sum = 0;
-    va_list args;
+        int i, sum = 0;
+        va_list args;
 
-    va_start(args, n);
+        va_start(args, n);
 
-    if (n == 0)
-        return (0);
+        if (n == 0)
+                return (0);
 
-    for (i = 0; i < n; i++)
-        sum += va_arg(args, int);
+        for (i = 0; i < n; i++)
+                sum += va_arg(args, int);
 
-    va_end(args);
+        va_end(args);
 
-    return (sum);
+        return (sum);
 }
