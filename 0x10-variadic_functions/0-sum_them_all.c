43be9e2395dc6f8bb086b35e69711946ce60f134
AmedBah <86324218+AmedBah@users.noreply.github.com>
2023-03-23 22:54:42 +0000

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,17 +1,24 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+/**
+ * Calculates the sum of all its integer parameters.
+ * 
+ * @param n The number of integer parameters to be summed.
+ * @param ... The integer parameters to be summed.
+ * 
+ * @return The sum of all the integer parameters.
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
-int i,sum = 0;
+int i, sum = 0;
 va_list args;
 va_start(args, n);
-    
+
 if (n == 0) 
-{
-return 0;
-}
+    return 0;
+
     
 for (i = 0; i < n; i++) 
 {
